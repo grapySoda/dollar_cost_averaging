16d9583501466ee574b1ae47837a55f8c43ec83f
grapysoda <grapysoda1951@gmail.com>
2024-05-18 23:01:19 +0800

    feat: add 5ma, 20ma, 60ma and 240ma in stock_database

---
+++ backtest/stock_database.py
@@ -0,0 +1,190 @@
+import os
+import datetime
+import pandas as pd
+from FinMind.data import DataLoader
+import matplotlib.pyplot as plt
+
+
+# from backtest.dealer import Dealer
+class LimitedArray:
+    def __init__(self, max_size):
+        self.window = []
+        self.array = []
+        self.max_size = max_size
+
+    def push(self, value):
+        if len(self.window) == self.max_size:
+            self.window.pop(0)
+        self.window.append(value)
+
+        if len(self.window) < self.max_size:
+            # self.array.append(0)
+            self.array.append(value)
+        else:
+            num = sum(self.window) / self.max_size
+            num = f"{num:.2f}"
+            self.array.append(num)
+
+
+class StockDatabase:
+
+    def __init__(self, stock_id, token):
+        self._start_date = datetime.date(1960, 4, 15)
+        self._end_date = datetime.date.today()
+        self._stock_id = stock_id
+        self._api = DataLoader()
+        self._api.login_by_token(api_token=token)
+
+        self.getPriceDatabase()
+        self.getDividendDatabase()
+
+    def plot(self, start_date, end_date):
+        mask = (self._price["date"] >= start_date) & (self._price["date"] <= end_date)
+        df_filtered = self._price.loc[mask]
+
+        # df_filtered.replace(0, pd.NA, inplace=True)
+        df_filtered.dropna(
+            subset=["5MA", "20MA", "60MA", "240MA"], how="all", inplace=True
+        )
+
+        plt.figure(figsize=(12, 6))
+
+        plt.plot(df_filtered["date"], df_filtered["close"], label="Close", linewidth=2)
+
+        plt.plot(df_filtered["date"], df_filtered["5MA"], label="5MA", linewidth=1)
+        plt.plot(df_filtered["date"], df_filtered["20MA"], label="20MA", linewidth=1)
+        plt.plot(df_filtered["date"], df_filtered["60MA"], label="60MA", linewidth=1)
+        plt.plot(df_filtered["date"], df_filtered["240MA"], label="240MA", linewidth=1)
+
+        plt.title("Stock Data")
+        plt.xlabel("Date")
+        plt.ylabel("Value")
+        plt.legend()
+
+        plt.show()
+
+    def str2date(self, date_str):
+        return datetime.datetime.strptime(date_str, "%Y-%m-%d").date()
+
+    def isSendDividendToday(self, input_date):
+        self._dividend["date"] = pd.to_datetime(self._dividend["date"])
+        if input_date in self._dividend["date"].astype(str).values:
+            print("Send")
+            return True
+        else:
+            return False
+
+    def getNextDividendDay(self, input_date):
+        _input_date = pd.to_datetime(input_date)
+
+        next_date = self._dividend[self._dividend["date"] > _input_date]["date"].min()
+
+        if pd.isnull(next_date):
+            return None
+
+        return self.str2date(next_date.strftime("%Y-%m-%d"))
+
+    def getDividendDatabaseTotalCash(self, input_date):
+        _input_date = pd.to_datetime(input_date)
+
+        closest_date_index = self._dividend["date"].sub(_input_date).abs().idxmin()
+        closest_date_row = self._dividend.loc[closest_date_index]
+
+        if closest_date_row["date"] != _input_date:
+            # next_date_row = self._dividend.loc[
+            #     self._dividend["date"] > _input_date
+            # ].iloc[0]
+            # return next_date_row["TotalCash"]
+            return 0.0
+        else:
+            return closest_date_row["TotalCash"]
+
+    def getMovingAverage(self, price):
+        _5ma = LimitedArray(5)
+        _20ma = LimitedArray(20)
+        _60ma = LimitedArray(60)
+        _240ma = LimitedArray(240)
+
+        maList = [_5ma, _20ma, _60ma, _240ma]
+        closeList = price["close"].tolist()
+
+        for close in closeList:
+            for ma in maList:
+                ma.push(close)
+
+        return _5ma.array, _20ma.array, _60ma.array, _240ma.array
+
+    def getPriceDatabase(self):
+        name = "taiwan_stock_daily"
+        filePath = "database/{}/{}.csv".format(name, self._stock_id)
+
+        if os.path.exists(filePath):
+            print("Find {}".format(filePath))
+            self._price = pd.read_csv(filePath)
+            self._price["date"] = pd.to_datetime(self._price["date"])
+        else:
+            print("Start to download {}".format(filePath))
+            self._price = self._api.taiwan_stock_daily(
+                stock_id=self._stock_id,
+                start_date=self._start_date,
+                end_date=self._end_date,
+            )
+
+            ### remove the empty price
+            self._price = self._price[self._price["close"] != 0]
+            self._price["date"] = pd.to_datetime(self._price["date"])
+
+            (
+                self._price["5MA"],
+                self._price["20MA"],
+                self._price["60MA"],
+                self._price["240MA"],
+            ) = self.getMovingAverage(self._price)
+
+            if not os.path.exists("database/{}".format(name)):
+                os.makedirs("database/{}".format(name))
+            self._price.to_csv(
+                "database/{}/{}.csv".format(name, self._stock_id),
+                index=False,
+            )
+
+    def getDividendDatabase(self):
+        name = "taiwan_stock_dividend"
+        filePath = "database/{}/{}.csv".format(name, self._stock_id)
+
+        if os.path.exists(filePath):
+            print("Find {}".format(filePath))
+            self._dividend = pd.read_csv(filePath)
+            self._dividend["date"] = pd.to_datetime(self._dividend["date"])
+        else:
+            print("Start to download {}".format(filePath))
+            self._dividend = self._api.taiwan_stock_dividend(
+                stock_id=self._stock_id,
+                start_date=self._start_date,
+            )
+
+            self._dividend["TotalStock"] = (
+                self._dividend["StockEarningsDistribution"]
+                + self._dividend["StockStatutorySurplus"]
+            )
+            self._dividend["TotalCash"] = (
+                self._dividend["CashEarningsDistribution"]
+                + self._dividend["CashStatutorySurplus"]
+            )
+            self._dividend = self._dividend[
+                [
+                    "date",
+                    "stock_id",
+                    "CashDividendPaymentDate",
+                    "TotalStock",
+                    "TotalCash",
+                ]
+            ]
+            self._dividend["date"] = pd.to_datetime(self._dividend["date"])
+
+            if not os.path.exists("database/{}".format(name)):
+                os.makedirs("database/{}".format(name))
+            self._dividend.to_csv(
+                "database/{}/{}.csv".format(name, self._stock_id),
+                index=False,
+            )
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from stock_database import StockDatabase
+from backtest.stock_database import StockDatabase
+
+# from backtest import Backtest
+
 import pandas as pd
 import datetime
 import os
 import time
 
-START_DATE = "1960-4-15"
-END_DATE = "2022-11-20"
+# START_DATE = "1960-4-15"
+# END_DATE = "2022-11-20"
+
+START_DATE = "2012-07-17"
+END_DATE = "2024-05-17"
 
 if __name__ == "__main__":
     try:
@@ -16,18 +22,28 @@ if __name__ == "__main__":
         print("Error: token.txt not found.")
         exit()
 
-    stock6208 = StockDatabase("006208", token)
+    stocks = {}
+    stocks["006208"] = StockDatabase("006208", token)
+    # stock6208 = StockDatabase("006208", token)
 
     date_iterator = pd.date_range(start=START_DATE, end=END_DATE, normalize=True)
     date_iterator = date_iterator.date
 
     # start_time = time.time()
-    nextDividendDay = stock6208.getNextDividendDay(START_DATE)
-    for date in date_iterator:
+    # nextDividendDay = stock6208.getNextDividendDay(START_DATE)
+    nextDividendDay = stocks["006208"].getNextDividendDay(START_DATE)
+    stocks["006208"].plot(START_DATE, END_DATE)
 
-        if date == nextDividendDay:
-            # print("{}, {}".format(date, stock6208.getDividendTotalCash(date)))
-            nextDividendDay = stock6208.getNextDividendDay(date)
+    # for date in date_iterator:
+
+    #     if date == nextDividendDay:
+    #         # print(
+    #         #     "{}, {}".format(
+    #         #         date, stocks["006208"].getDividendDatabaseTotalCash(date)
+    #         #     )
+    #         # )
+    #         # nextDividendDay = stock6208.getNextDividendDay(date)
+    #         nextDividendDay = stocks["006208"].getNextDividendDay(date)
 
     # end_time = time.time()
     # print("Excution time：", end_time - start_time, " sec")
